2c7b8bd1730d09f3c5c304f60cc973768481bdc5
Arose Niazi <arose.niazi@yahoo.com>
2021-06-10 05:01:00 +0500

    API and front end complete

---
+++ API/index.js
@@ -0,0 +1,73 @@
+const e = require("express");
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+app.use(express.json());
+
+let facultyID = data.length;
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, DELETE"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.send(
+        "Error: Please use the faculty API using " +
+            req.baseUrl +
+            "/api/faculty"
+    );
+});
+
+app.get("/api/faculty", function (req, res) {
+    res.json(data);
+});
+
+app.get("/api/faculty/:index", function (req, res) {
+    for (let index = 0; index < data.length; index++) {
+        if (data[index].id == req.params.index) return res.json(data[index]);
+    }
+    return res.status(400).send("Faculty member not found");
+});
+
+app.put("/api/faculty/:index", function (req, res) {
+  for (let index = 0; index < data.length; index++) {
+      if (data[index].id == req.params.index) 
+    {
+      var keys = Object.keys(req.body);
+      for (let key = 0; key < keys.length; key++) {
+          data[index][keys[key]] = req.body[keys[key]];
+      }
+      return res.json(data[index]);
+    }
+  }
+  return res.status(400).send("Faculty member not found");
+});
+//delete one resource
+app.delete("/api/faculty/:index", function (req, res) {
+  for (let index = 0; index < data.length; index++) {
+    if (data[index].id == req.params.index) {
+      data.splice(index, 1);
+      return res.status(200).send("Removed!");
+    }
+  }
+  return res.status(400).send("Faculty member not found");
+});
+//create one resource
+app.post("/api/faculty", function (req, res) {
+    console.log();
+    data.push(req.body);
+    data[data.length - 1].id = facultyID++;
+    res.send(data);
+});
+
+app.get("*", function (req, res) {
+    res.send("Invalid API", 404);
+});
+
+app.listen(3000);
---
+++ jqcrud.js
@@ -0,0 +1,142 @@
+$(function () {
+    loadMembers();
+    $("#faculty").on("click", ".btn-danger", handleDelete);
+    $("#faculty").on("click", ".btn-warning", handleUpdate);
+    $("#addBtn").click(addMember);
+    $("#updateSave").click(preformUpdate);
+});
+
+function preformUpdate() {
+    var id = $("#updateId").val();
+    var name = $("#Uname").val();
+    var gender = $("#Ugender").val();
+    var age = $("#Uage").val();
+    var email = $("#Uemail").val();
+    var country = $("#Ucountry").val();
+    var city = $("#Ucity").val();
+    var street_address = $("#Ustreet_address").val();
+    var address = {country: country, city: city, street_address: street_address};
+    var course_code = $("#Ucourse_code").val();
+    var phone_numbers = $("#Uphone_numbers").val();
+    phone_numbers = phone_numbers.split(",");
+    var tosend = JSON.stringify({
+        name,
+        gender,
+        age,
+        email,
+        address,
+        course_code,
+        phone_numbers,
+    });
+    $.ajax({
+        url: "https://assign3api.arose-niazi.me/api/faculty/" + id,
+        headers: { "Content-Type": "application/json" },
+        dataType: "json",
+        data: tosend,
+        method: "PUT",
+        success: function (response) {
+            console.log(response);
+            loadMembers();
+            $("#updateModal").modal("hide");
+        },
+    });
+}
+function handleUpdate() {
+    var btn = $(this);
+    var parentDiv = btn.closest(".member");
+    let id = parentDiv.attr("data-id");
+    $.get("https://assign3api.arose-niazi.me/api/faculty/" + id, function (response) {
+        $("#updateId").val(response.id);
+        $("#Uname").val(response.name);
+        $("#Ugender").val(response.gender);
+        $("#Uage").val(response.age);
+        $("#Uemail").val(response.email);
+        $("#Ucountry").val(response.address.country);
+        $("#Ucity").val(response.address.city);
+        $("#Ustreet_address").val(response.address.street_address);
+        $("#Ucourse_code").val(response.course_code);
+        $("#Uphone_numbers").val(response.phone_numbers);
+        $("#updateModal").modal("show");
+    });
+}
+function addMember() {
+    var name = $("#name").val();
+    var gender = $("#gender").val();
+    var age = $("#age").val();
+    var email = $("#email").val();
+    var country = $("#country").val();
+    var city = $("#city").val();
+    var street_address = $("#street_address").val();
+    var address = {country: country, city: city, street_address: street_address};
+    var course_code = $("#course_code").val();
+    var phone_numbers = $("#phone_numbers").val();
+    phone_numbers = phone_numbers.split(",");
+    var tosend = JSON.stringify({
+        name,
+        gender,
+        age,
+        email,
+        address,
+        course_code,
+        phone_numbers,
+    });
+    //console.log("Sending data" +tosend);
+    //console.table(tosend);
+    $.ajax({
+        url: "https://assign3api.arose-niazi.me/api/faculty",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        dataType: "json",
+        data: tosend,
+        success: function (response) {
+            console.log(response);
+            $("#name").val("");
+            $("#gender").val("");
+            $("#age").val("");
+            $("#email").val("");
+            $("#country").val("");
+            $("#city").val("");
+            $("#street_address").val("");
+            $("#course_code").val("");
+            $("#phone_numbers").val("");
+            loadMembers();
+            $("#addModal").modal("hide");
+        },
+    });
+}
+function handleDelete() {
+    var btn = $(this);
+    var parentDiv = btn.closest(".member");
+    let id = parentDiv.attr("data-id");
+    console.log(id);
+    $.ajax({
+        url: "https://assign3api.arose-niazi.me/api/faculty/" + id,
+        method: "DELETE",
+        success: function () {
+            loadMembers();
+        },
+    });
+}
+function loadMembers() {
+    $.ajax({
+        url: "https://assign3api.arose-niazi.me/api/faculty",
+        method: "GET",
+        error: function (response) {
+            var faculty = $("#faculty");
+            faculty.html("An Error has occured");
+        },
+        success: function (response) {
+            console.log(response);
+            var faculty = $("#faculty");
+            faculty.empty();
+            for (var i = 0; i < response.length; i++) {
+                var rec = response[i];
+                numbers = rec.phone_numbers;
+                faculty.append(
+                    `<div class="member" data-id="${rec.id}"><h3>${rec.name}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button>Age: ${rec.age} Gender: ${rec.gender}</br>Email: ${rec.email}</br>Address:<div class="address"><br>${rec.address.country} ---  ${rec.address.city}</br> ${rec.address.street_address}</div></br>Phone: ${numbers}</p></div>`
+                );
+                // faculty.append("<div><h3>" + rec.title + "</h3></div>");
+            }
+        },
+    });
+}
